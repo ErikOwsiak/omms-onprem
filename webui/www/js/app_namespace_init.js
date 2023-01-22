6e0ddeaa604a48c5ae3c7aae836fbf7649c02882
erik <erik@iotech.systems>
2023-01-22 23:47:53 +0100

    added _init_data

---
+++ webui/www/js/app_namespace_init.js
@@ -0,0 +1,11 @@
+
+/* app main namespace */
+var _omms = { 
+   LOAD_DTS_UTC: null,
+   __omms_init() {
+      _omms.LOAD_DTS_UTC = Date.now();
+   }
+};
+
+/* init  namespace */
+_omms.__omms_init();
