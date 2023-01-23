b77a420abded0ead7017252b230ea52d1238b1bf
erik <erik@iotech.systems>
2023-01-23 23:27:28 +0100

    conn fix

--- core/paramikoOps.py
+++ core/paramikoOps.py
@@ -49,6 +49,7 @@ class paramikoOps(object):
 
    def __run_cmd(self, cinfo, cmd) -> []:
       shell: Channel = self.ssh_clt.invoke_shell()
+      time.sleep(2.0)
       shell.in_buffer.empty()
       err, buff = self.__cmd_shell_resp(shell, cmd)
       return buff.splitlines()
