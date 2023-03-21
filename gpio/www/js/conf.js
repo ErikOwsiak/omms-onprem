90190a346cc5c638a0b2aaa46a148eed4b74e6e8
erik <erik@iotech.systems>
2023-03-22 00:24:12 +0100

    adding building gpio

--- gpio/www/js/conf.js
+++ gpio/www/js/conf.js
@@ -1,6 +1,8 @@
 
 class gpioConf {
 
+   static urlpfx = "/omms/gpio";
+
    constructor() {
       this.btnOn = null;
       this.btnOff = null;
@@ -12,20 +14,23 @@ class gpioConf {
 
    init() {
       /* force on button */
-      // this.btnOn = document.getElementById("btnOverrideON");
       this.btnOn = this.doc.byID("btnOverrideON");
       this.btnOn.addEventListener("click", this.forceOn);
       /* force off button */
-      // this.btnOff = document.getElementById("btnOverrideOFF");
-      this.btnOff = this.elmByID("btnOverrideOFF");
+      this.btnOff = this.doc.byID("btnOverrideOFF");
       this.btnOff.addEventListener("click", this.forceOff);
       /* save config info */
-      this.btnSave = document.getElementById("btnSave");
+      this.btnSave = this.doc.byID("btnSave");
       this.btnSave.addEventListener("click", this.saveConf);
    }
 
    forceOn() {
       console.log("fon");
+      const devid = "devid";
+      const chnl = "chnl";
+      const state = "on";
+      const url = `${gpioConf.urlpfx}/force/${devid}/${chnl}/${state}`;
+      fetch(url).then((rsp) => rsp.text()).then((d) => console.log(d));
    }
 
    forceOff() {
