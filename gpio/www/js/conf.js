84227d62100f43d3f1b99a61d642d148deb5395b
erik <erik@iotech.systems>
2023-03-21 23:23:02 +0100

    adding building gpio

--- gpio/www/js/conf.js
+++ gpio/www/js/conf.js
@@ -2,11 +2,30 @@
 class gpioConf {
 
    constructor() {
-
+      this.btnOn = null;
+      this.btnOff = null;
+      this.btnSave = null;
    }
 
    init() {
-      alert("init..");
+      this.btnOn = document.getElementById("btnOverrideON");
+      this.btnOn.addEventListener("click", this.forceOn);
+      this.btnOff = document.getElementById("btnOverrideOFF");
+      this.btnOff.addEventListener("click", this.forceOff);
+      this.btnSave = document.getElementById("btnSave");
+      this.btnSave.addEventListener("click", this.saveConf);
+   }
+
+   forceOn() {
+      console.log("fon");
+   }
+
+   forceOff() {
+      console.log("off");
+   }
+
+   saveConf() {
+      console.log("save");
    }
 
 };
