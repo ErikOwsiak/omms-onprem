e34e0117dc46b41e9f162fc56de42dc4b268eab1
erik <erik@iotech.systems>
2023-03-24 17:04:53 +0100

    added dev relays xml file for testing

--- gpio/www/js/html.js
+++ gpio/www/js/html.js
@@ -12,13 +12,12 @@ const HTML = {
       hrs.push(`</optgroup><optgroup label="DayParts">`);
       hrs.push(`<option value="sunrise">Sunrise</option>`);
       hrs.push(`<option value="sunset">Sunset</option>`);
-      hrs.push("</optgroup>");
+      hrs.push("</optgroup></select>");
       /* -- */
-      hrs.push("</select>");
       let m, mnts = [`<select id="selMM_${id}" class="${cls}">`] 
       for (let i = 0; i <= 45; i += 15) {
          m = String(i).padStart(2, "0");
-         mnts.push(`<option value="${i}">${i}</option>`);
+         mnts.push(`<option value="${m}">${m}</option>`);
       }
       /* -- */
       mnts.push("</select>");
