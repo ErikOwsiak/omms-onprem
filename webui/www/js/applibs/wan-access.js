277cf8678b7d841dd731552bd318ebb8dd78a626
erik <erik@iotech.systems>
2023-03-29 21:07:54 +0200

    adding lab code

--- webui/www/js/applibs/wan-access.js
+++ webui/www/js/applibs/wan-access.js
@@ -9,8 +9,11 @@ class wanAccess {
 
    init() {
       alert("xxxxx");
+      this.ctls = `<input type="text" id="waNums" value="" placeholder="enter number hours/days"/>` + 
+         `<select id="waNumTypes"><option value="h">hours</option><option value="d">days</option></select>`
+         `<input id="bntCreateAccessQRC" type="button" value="CreateAccessQRC" />`;
       this.frame = `<div class="wa-frame"><div class="wa-qac"></div>` +
-         `<div id="waCtrls" class="wa-ctls"></div></div>`;
+         `<div id="waCtrls" class="wa-ctls">${this.ctls}</div></div>`;
       $("#subMenuCol").html(this.frame);
    }
 
